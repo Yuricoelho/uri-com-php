a122fb3b0c683055cc2e0cbfdb0519479391cc03
Yuri <yuricoelho33@gmail.com>
2021-08-03 20:17:06 -0300

    Resolução do exercício 1007 do URI.

---
+++ uri-1007.php
@@ -0,0 +1,11 @@
+<?php
+
+$A = (int) readline();
+$B = (int) readline();
+$C = (int) readline();
+$D = (int) readline();
+
+$diferencaProduto = $A * $B - $C * $D;
+
+printf("DIFERENCA = %d\n", $diferencaProduto);
+
